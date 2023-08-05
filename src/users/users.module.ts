c5fd617e9402f0e0e92b6c893e4c951c0cc0dcb7
Shubham-5547 <shubhamsoni555p@gmail.com>
2023-08-05 23:49:50 +0530

    preproduction commit

---
+++ src/users/users.module.ts
@@ -0,0 +1,24 @@
+import { Module, MiddlewareConsumer } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
+import { User } from './user.entity';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [UsersService, AuthService,
+  {
+    provide: APP_INTERCEPTOR,
+    useClass: CurrentUserInterceptor
+  }],
+})
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
